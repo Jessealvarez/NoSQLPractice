0922de4cfe91a5d3cf13c050d4ca24fc5ef38579
Jesse Alvarez <jesse@Jesses-MBP.home>
2022-05-24 14:54:01 -0400

    first commit

---
+++ NoSQLPractice.js
@@ -0,0 +1,20 @@
+// let newBlog = {
+//     createdAt: new Date().toISOString(),
+//     title: "New Blog",
+//     text: "testing",
+//     author: "Jesse",
+//     id: String(db.blogposts.count() + 1)
+// }
+
+// db.blogposts.insertOne(newBlog);
+
+//  db.blogposts.find({author: "Jesse"})
+
+// db.blogposts.updateOne(
+//     {title: "New Blog"},
+//     {$set: {title: "New Blog Title"}})
+
+//   db.blogposts.deleteOne(
+//     {title: "New Blog Title"})
+
+
